43c954d5bff9a11711e4dac529309099d3e1322f
Kalaiyarasan00 <139617393+Kalaiyarasan00@users.noreply.github.com>
2023-09-01 00:02:13 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1590,7 +1590,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
     elif query.data == "tele":
             btn = [[
                     InlineKeyboardButton("⟸ Bᴀᴄᴋ", callback_data="help"),
-                    InlineKeyboardButton("Cᴏɴᴛᴀᴄᴛ", url="telegram.me/creatorrio")
+                    InlineKeyboardButton("Cᴏɴᴛᴀᴄᴛ", url="http://t.me/Culprit_003")
                   ]]
             await client.edit_message_media(
                 query.message.chat.id, 
