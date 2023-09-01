aaa9e37e0fe0e8a701d5710dbc34645ca38fba0e
Kalaiyarasan00 <139617393+Kalaiyarasan00@users.noreply.github.com>
2023-09-01 23:24:30 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -92,7 +92,7 @@ async def start(client, message):
         await client.send_photo( 
             chat_id=message.from_user.id,
             photo="https://graph.org/file/3e4a86d1838c5ced4b92f.jpg"
-            caption="**You are not in our channel given below so you don't get the movie file...\n\nIf you want the movie file, click on the '🍿ᴊᴏɪɴ ᴏᴜʀ ʙᴀᴄᴋ-ᴜᴘ ᴄʜᴀɴɴᴇʟ🍿' button below and join our back-up channel, then click on the '🔄 Try Again' button below...\n\nThen you will get the movie files...**",
+            caption="You are not in our channel given below so you don't get the movie file...\n\nIf you want the movie file, click on the '🍿ᴊᴏɪɴ ᴏᴜʀ ʙᴀᴄᴋ-ᴜᴘ ᴄʜᴀɴɴᴇʟ🍿' button below and join our back-up channel, then click on the '🔄 Try Again' button below...\n\nThen you will get the movie files...",
             reply_markup=InlineKeyboardMarkup(btn),
             parse_mode=enums.ParseMode.MARKDOWN
             ) 
